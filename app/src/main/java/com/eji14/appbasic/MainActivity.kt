22d56eaf1c83e4d6422372d4194a3fdf03b37d12
mohalfarizi <mohalfarizi.eji@gmail.com>
2026-05-06 10:24:39 +0800

    update

--- app/src/main/java/com/eji14/appbasic/MainActivity.kt
+++ app/src/main/java/com/eji14/appbasic/MainActivity.kt
@@ -1,32 +1,14 @@
 package com.eji14.appbasic
 
-import ShowCaseScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.eji14.appbasic.ui.screen.DialogExampleScreen
-import com.eji14.appbasic.ui.screen.TextEditorScreen
-import com.eji14.appbasic.ui.screen.TextShowCase
-import com.eji14.appbasic.ui.theme.AppBasicTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            AppBasicTheme {
-//                TextEditorScreen {  }
-//                ExampleDialogScreen()
-                DialogExampleScreen()
-            }
-        }
+        setContent {}
     }
 }
